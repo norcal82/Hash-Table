01b1e4000b17003794bf3952f6b4ed94be1d2ed5
Clint Bettiga <norcal82@gmail.com>
2013-11-07 19:20:40 -0800

    Update hash_function.cpp

--- hash_function.cpp
+++ hash_function.cpp
@@ -14,26 +14,18 @@ int
 Hash::hf ( string ins ) {
   
 
- //  int hash = 5381;
-
- //  for(unsigned int i = 0; i < ins.length(); i++)
- //  	hash = ((hash << 5) + hash) + ins.at(i);
- //  // place your hash function here....
- //  return hash % HASH_TABLE_SIZE;
-
-	// return ( (int) ins[0] ) % HASH_TABLE_SIZE;
 
 	//REFRENCE
 	//FNV-1a Hash (http://isthe.com/chongo/tech/comp/fnv/) in C++.
-
+	
 	uint32 hval = FNV1_32A_INIT;
-
-
+	
+	
 	for(unsigned int i = 0; i < ins.length(); i++){
-  	  hval ^= (uint32)ins.at(i);
-      hval *= FNV_32_PRIME;
+		hval ^= (uint32)ins.at(i);
+		hval *= FNV_32_PRIME;
 	}
-
+	
 	return hval % HASH_TABLE_SIZE;
 }
 
